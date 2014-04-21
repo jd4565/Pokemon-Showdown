33ec99dcb980853b2cc6f990dbcaf4f40012e950
Atomichaos <Hunter15973@gmail.com>
2014-04-21 18:13:52 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1902,8 +1902,8 @@ var commands = exports.commands = {
         	this.sendReplyBox('<center><img height="120" src="http://31.media.tumblr.com/2beffec8c82815d7ce9bced0596bbeb4/tumblr_ms5xy54W4b1s1syfso1_500.gif">' +
         		'<img src="http://i.imgur.com/kzCkWh2.png" height="75">' +
         		'<img src="http://24.media.tumblr.com/eae21cecedab8952379a03c023388625/tumblr_mqb3o9scfu1qcrbfzo1_500.gif" height="100"><br />' +
-        		'<b>Ace: </b>Pikachu<br />' +
-        		'<b>Catchphrase: </b>Screw Catchphrases</center>');
+        		'<b>Ace: </b>Greninja<br />' +
+        		'<b>Catchphrase: </b>Screw catchphrases I'm a ninja</center>');
     },
                    
    	missing1 : 'missing1',
