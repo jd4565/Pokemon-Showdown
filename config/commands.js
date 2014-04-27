a125fbf9e3b575c591ae825d06e12f0bd3031ad6
Atomichaos <Hunter15973@gmail.com>
2014-04-27 00:06:41 -0400

    Update commands.js

--- config/commands.js
+++ config/commands.js
@@ -1929,8 +1929,8 @@ var commands = exports.commands = {
    	haruko : 'haruko',
    	haruko: function(target, room, user) {
         	if (!this.canBroadcast()) return;
-        	this.sendReplyBox('<center><img height="120" src="http://31.media.tumblr.com/2beffec8c82815d7ce9bced0596bbeb4/tumblr_ms5xy54W4b1s1syfso1_500.gif">' +
-        		'<img src="http://i.imgur.com/kzCkWh2.png" height="50">' +
+        	this.sendReplyBox('<center><img height="100" src="http://31.media.tumblr.com/2beffec8c82815d7ce9bced0596bbeb4/tumblr_ms5xy54W4b1s1syfso1_500.gif">' +
+        		'<img src="http://i.imgur.com/kzCkWh2.png" height="60">' +
         		'<img src="http://24.media.tumblr.com/eae21cecedab8952379a03c023388625/tumblr_mqb3o9scfu1qcrbfzo1_500.gif" height="100"><br />' +
         		'<b>Ace: </b>Greninja<br />' +
         		'<b>Catchphrase: </b>Screw catchphrases Im a ninja</center>');
@@ -1977,7 +1977,7 @@ var commands = exports.commands = {
       	hallie: function(target, room, user) {
         	if (!this.canBroadcast()) return;
         	this.sendReplyBox('<center><img height="100" src="http://www.pkparaiso.com/imagenes/xy/sprites/animados/gyarados-mega.gif">' +
-        		'<img src=http://i.imgur.com/vI6Y91f.gif" height="75">' +
+        		'<center><img src=http://i.imgur.com/vI6Y91f.gif" height="75">' +
         		'<img src="http://i.imgur.com/nhOPMlk.png" height="100"><br />' +
         		'<b>Ace: </b>Gyarados<br />' +
         		'<b>Catchphrase: </b>"I will always flow, water by water"</center>');
