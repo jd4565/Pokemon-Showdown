7d7bda9d8f51e9e8b7e927a6719e6145071d2758
SilverTactic <brahadeeshk@gmail.com>
2014-04-30 21:48:20 +0530

    slowmons clause fix
    Slowmons was bugged. Now fixed. Sorry for the inconvenience

--- data/rulesets.js
+++ data/rulesets.js
@@ -208,16 +208,16 @@ exports.BattleFormats = {
 		}
 	},
 	slowmonsclause: {
- 	effecttype: 'Rule',
- 	onStart: function(target, source) {
-                                 this.add('rule', 'SlowMons Clause: Dimensions are twisted by default.','-fieldstart', 'move: Trick Room', '[of] '+source);
- 								this.add('-message', 'The dimensions were twisted!');
-                                 this.getStatCallback = function(stat, statName) {
- 
-                                         return stat;
- 										}
- 										}
- 										},
+	effecttype: 'Rule',
+	onStart: function(target, source) {
+                                this.add('rule', 'SlowMons: Trick Room exists by default.','-fieldstart', 'move: Trick Room', '[of] '+source);
+								this.add('-message', 'The dimensions were twisted!');
+                                this.getStatCallback = function(stat, statName) {
+					if (statName === 'spe') return -stat;
+					return stat;
+					}
+										}
+										},
 	cappokemon: {
 		effectType: 'Rule',
 		validateSet: function (set, format) {
